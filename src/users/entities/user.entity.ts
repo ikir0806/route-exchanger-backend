b1a0e6a41947b43ee7b085026edbafacd26db468
ikir0806 <ikir2826@gmail.com>
2024-05-30 11:46:15 +0300

    уникальные поля

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,17 +2,20 @@ import { RouteEntity } from 'src/routes/entities/route.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('users')
+// @Index(['email', 'login'], { unique: true })
+// @Unique(['email', 'login'])
+// @Unique('UQ_NAMES', ['email', 'login'])
 export class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ unique: true })
   email: string;
 
   @Column()
   password: string;
 
-  @Column()
+  @Column({ unique: true })
   login: string;
 
   @Column()
