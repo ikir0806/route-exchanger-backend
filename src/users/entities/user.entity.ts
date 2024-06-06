773ed8352d86a8557cad3c5b9f7d067733dffa2f
Kirill Makarov <ikir2826@gmail.com>
2024-06-06 17:01:16 +0300

    чистка комментариев

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,9 +2,6 @@ import { RouteEntity } from 'src/routes/entities/route.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('users')
-// @Index(['email', 'login'], { unique: true })
-// @Unique(['email', 'login'])
-// @Unique('UQ_NAMES', ['email', 'login'])
 export class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
